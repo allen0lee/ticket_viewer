a7445ace0de8e4253334eb71b82c353b27df5343
Alan <allen0lee@gmail.com>
2021-05-04 14:50:29 +1000

    need a helper function to assist authentication

--- controllers/tickets_controller.rb
+++ controllers/tickets_controller.rb
@@ -6,10 +6,31 @@ require_relative '../models/ticket.rb'
 
 class TicketViewer < Sinatra::Application
   get "/" do
-
-    p 'hello 1'
-    # binding.pry
-    erb(:error)
+    begin
+      page_number = 1
+      show_tickets_list(page_number)
+      erb(:tickets_list)
+    rescue => ex
+      @error_message = ex.message
+      erb(:error)
+    end  
   end
+
+
+  def show_tickets_list(page_number)
+    @page_number = page_number
+    @tickets_shown_per_page = 25
+    url = "https://alanli.zendesk.com/api/v2/tickets.json?page=#{@page_number}&per_page=#{@tickets_shown_per_page}"
+
+    # res = AuthenticationHelper.make_req_to_api(url)
+    
+    # @tickets = find_tickets(res)
+
+
+    # @count = res["count"]
+    # @pages = (@count.to_f / @tickets_shown_per_page).ceil
+  end
+
+
 
 end
