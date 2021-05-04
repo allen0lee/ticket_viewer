33e0e43ee6c775d5e8fb1a389b0e2bedb363387a
Alan <allen0lee@gmail.com>
2021-05-04 13:02:10 +1000

    create controller and main program, test main

---
+++ main.rb
@@ -0,0 +1,8 @@
+require 'sinatra'
+require 'sinatra/reloader' if settings.development?
+require_relative 'controllers/tickets_controller.rb'
+
+class TicketViewer < Sinatra::Application
+end
+
+TicketViewer.new
